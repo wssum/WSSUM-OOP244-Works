0a70d4571cb0e49fe5ed64c17ed1118e11492ac7
Wilson Sum <wssum@myseneca.ca>
2023-07-04 18:03:07 -0400

    MS1 and MS2(Pre-Submit)

---
+++ MS Final Proj/ms2/LibApp.cpp
@@ -0,0 +1,235 @@
+/*/////////////////////////////////////////////////////////////////////////
+
+Workshop - #MS2
+
+Full Name :Wilson Sum
+
+Student ID#:017142142
+
+Email :wssum@myseneca.ca
+
+Section :OOP244NAA
+
+Section :OOP244NAA
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+
+shared with any other student or 3rd party content provider. This submitted
+
+piece of work is entirely of my own creation.
+
+/////////////////////////////////////////////////////////////////////////*/
+
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.cpp
+// Version
+// Date
+// Author
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iomanip>
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+#include "UtilsB.h"
+#include "LibApp.h"
+
+namespace sdds {
+	//With this method the one arg constructor from menu class is called multiple times to initialize the titles of the Menu type objects within the LibApp Object.
+	LibApp::LibApp() : m_mainMenu("Seneca Library Application"), m_exitMenu("Changes have been made to the data, what would you like to do?")//<<<<(This Method)
+	{
+		m_changed = false;
+		/*Using the << overload operator the Items from the Menu class is initialized with multiple strings.*/
+		m_mainMenu << "Add New Publication" << "Remove Publication" << "Checkout publication from library" << "Return publication to library";
+		/*Same as above.*/
+		m_exitMenu << "Save changes and exit" << "Cancel and go back to the main menu";
+	}
+
+	LibApp::~LibApp()
+	{
+
+	}
+
+	void LibApp::load()
+	{
+		cout << "Loading Data" << endl;
+	}
+
+	void LibApp::save()
+	{
+		cout << "Saving Data" << endl;
+	}
+
+	void LibApp::search()
+	{
+		cout << "Searching for publication" << endl;
+	}
+
+	bool LibApp::confirm(const char* message)
+	{
+		Menu tempMenu(message);//Makes temp menu obj with parameter message to represent a question to ask for confirmation.
+		int select = -1;//The variable that will hold the result.
+
+		tempMenu << "yes";//Using the << overload operator yes is the only given MenuItem as well as the mandatory exit from the menu.
+
+		select = tempMenu.run();//Option chosen by user will be assigned to select variable and be evaluated to return the proper bool.
+
+		if (select == 1)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void LibApp::newPublication()
+	{
+		bool decider = false;//Bool variable to hold decision of confirm.
+
+		cout << "Adding new publication to library" << endl;
+
+		decider = confirm("Add this publication to library?");//Calling confirm function to decide whether or not to add a item to publication and assigning the bool result to decider.
+
+		if (decider == true)//If decider is true then the value of m_changed will become true to signify change.
+		{
+			m_changed = true;
+			cout << "Publication added" << endl;
+		}
+
+	}
+
+
+	void LibApp::removePublication()
+	{
+		bool decider = false;//Bool variable to hold decision of confirm.
+
+		cout << "Removing publication from library" << endl;
+
+		search();
+
+		decider = confirm("Remove this publication from the library?");//Calling confirm function to decide whether or not to remove a item to publication and assigning the bool result to decider.
+
+		if (decider == true)//If decider is true then the value of m_changed will become true to signify change.
+		{
+			m_changed = true;
+			cout << "Publication removed" << endl;
+		}
+
+	}
+
+	void LibApp::checkOutPub()
+	{
+		bool decider = false;//Bool variable to hold decision of confirm.
+
+		search();
+
+		decider = confirm("Check out publication?");//Calling confirm function to decide whether or not to checkout a item to publication and assigning the bool result to decider.
+
+		if (decider == true)//If decider is true then the value of m_changed will become true to signify change.
+		{
+			m_changed = true;
+			cout << "Publication checked out" << endl;
+		}
+
+	}
+
+	void LibApp::returnPub()
+	{
+		search();
+		cout << "Returning publication" << endl;
+		cout << "Publication returned" << endl;
+		m_changed = true;
+	}
+
+	void LibApp::run()
+	{
+		bool yRn = false; //Bool variable to hold confirmation value for exiting without saving changes.
+		int selection = -1, subSelect = -1;//Variables to hold selections and subselection decisions from user that will determine the actions of this program.
+		int flag = 0;
+
+		while (flag == 0)//A while loop that wil continue running till a complete exit has been confirmed, this exit path may vary.
+		{
+			selection = m_mainMenu.run();//Using the run function from the m_mainMenu object within the LibApp class a selection will be made.
+
+			/*A list of if statements separated as if statements rather than if else because this method will confuse teh compiler alot less.
+			These if statements will trigger certain functions depending on the selection made by the user*/
+			if (selection == 1)
+			{
+				newPublication();
+			}
+
+			if (selection == 2)
+			{
+				removePublication();
+			}
+
+			if (selection == 3)
+			{
+				checkOutPub();
+			}
+
+			if (selection == 4)
+			{
+				returnPub();
+			}
+
+			/*If selection 0 is selected which signifies an exit is triggered the m_changed data member wil be evaluated to see if
+			there was a change and if so a bunch of other options will be given to the user.*/
+			if (selection == 0)
+			{
+				if (m_changed == false)
+				{
+					flag = 1;
+				}
+
+				if (m_changed == true)
+				{
+					subSelect = m_exitMenu.run();
+					/*If the user did change something the m_exitMenu will run it's run function that lists out more options and will be determined by
+					the subselect variable which will hold the decision of the user.*/
+					if (subSelect == 1)
+					{
+						save();//Option 1 will save all changes and then terminate the program by seeting flag to 1.
+						flag = 1;
+					}
+
+					if (subSelect == 2)
+					{
+						selection = -1;//Option 2 will set both variables selection and subSelect back to their safe empty positions and then
+						subSelect = -1;//since flag is still 0 will continue the program going back to the main menu.
+					}
+
+					if (subSelect == 0)
+					{
+						 yRn = confirm("This will discard all the changes are you sure?");
+						 /*If 0 is selcted a confirmation will be made to make sure that the user trully wants to exit completely
+						   without saving as option 0 will simply exit without making any changes whatsoever. The confirmation
+						   will be from the confirm function and the result will be assigned to yRn which will then be validated.*/
+						 if (yRn == true)
+						 {
+							 flag = 1;//If the user chooses to exit without saving the flag will be set to 1 which will terminate the program.
+						 }
+						 else
+						 {
+							 selection = -1;//If the user wishes not to exit, they will be led back to the main screen and variables selection
+							 subSelect = -1;//and subSelect will be set back to their original position to be reused.
+						 }
+					}
+				}
+				
+			}
+		}
+		cout << "GoodBye." << endl;
+	}
+
+}
---
+++ MS Final Proj/ms2/MenuB.cpp
@@ -0,0 +1,283 @@
+/*/////////////////////////////////////////////////////////////////////////
+
+Workshop - #MS1
+
+Full Name :Wilson Sum
+
+Student ID#:017142142
+
+Email :wssum@myseneca.ca
+
+Section :OOP244NAA
+
+Section :OOP244NAA
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+
+shared with any other student or 3rd party content provider. This submitted
+
+piece of work is entirely of my own creation.
+
+/////////////////////////////////////////////////////////////////////////*/
+
+// Final Project Milestone 1 
+// Date Module
+// File	Date.cpp
+// Version 1.0
+// Author	Fardad Soleimanloo
+// Revision History
+// -----------------------------------------------------------
+// Name               Date                 Reason
+// 
+/////////////////////////////////////////////////////////////////
+#define _CRT_SECURE_NO_WARNINGS
+#include <iomanip>
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+#include "MenuB.h"
+
+namespace sdds {
+
+	/*MenuItem funcs*/
+
+	MenuItem::MenuItem(const char* name)
+	{
+		/*Checks to see if there is a valid argument passed to the constructor and if so memory decided by the
+		length of the arg will be allocated and the name parameter will be copied to Name data memeber.*/
+		if (name)
+		{
+			Name = new char[strlen(name) + 1];
+			strcpy(Name, name);
+		}
+		else//If no valid argument the Name data member will be set to a safe null state to prevent any errors.
+		{
+			Name = nullptr;
+		}
+
+	}
+
+	MenuItem::MenuItem()
+	{
+		Name = nullptr;//The Name data member will be set to a safe emtpy state to prevent any errors from occuring.
+	}
+
+	MenuItem::~MenuItem()
+	{
+		delete[] Name;//Deconstructor deletes the Name pointer.
+	}
+
+	MenuItem::operator bool()
+	{
+		/*If the current objects Name data member is not null then this operator will return true
+		otherwise it will return false to signify that this object has not a valid name.*/
+		if (Name)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	MenuItem::operator const char* ()const
+	{
+		/*Returns the Name data member to be casted and used as a const char pointer.*/
+		return Name;
+	}
+
+	ostream& MenuItem::displayName(std::ostream& os)
+	{
+		/*Checks to see if the object is not null and if so will output the data member
+		representing the name of the object to the ostream object selected as the parameter
+		which will then be returned as the value*/
+		if (*this)
+		{
+			os << Name;
+		}
+		return os;
+	}
+
+	/*Menu funcs*/
+	Menu::Menu()
+	{
+		amtOfItems = 0;//Initializes the amtOfItems as empty.              
+
+		menuTitle = nullptr;//Sets the menuTitle pointer to a safe empty state to prevent any errors.
+
+		for (int i = 0; i < MAX_MENU_ITEMS; i++)//Goes through the array MenuItems and sets them to a safe empty state to prevent any errors.
+		{
+			Items[i] = nullptr;
+		}
+	}
+
+	Menu::Menu(const char* arg)
+	{
+		/*Allocates memory for the MenuItem pointer, using the char cast operator from MenuItem class
+		and initializes its Name data member with the arg parameter reprpesenting the name*/
+		menuTitle = new MenuItem(arg);
+
+		for (int i = 0; i < MAX_MENU_ITEMS; i++)//Goes through the array MenuItems and sets them to a safe empty state to prevent any errors.
+		{
+			Items[i] = nullptr;
+		}
+
+		amtOfItems = 0;//Initializes the amtOfItems as empty.
+	}
+
+
+
+	Menu::~Menu()
+	{
+		for (int i = 0; i < MAX_MENU_ITEMS; i++)//Goes through the array MenuItems and deletes them to prevent memory leaks.
+		{
+			delete Items[i];
+		}
+
+		delete menuTitle;//Deletes the menuTitle to prevent memory leaks.
+	}
+
+	/*Returns true if the amtOfItems variable is more than 0 meaning its not empty.*/
+	Menu::operator bool()
+	{
+		if (amtOfItems > 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	/*Returns the amtOfItems as a unsigned int*/
+	Menu::operator unsigned int()
+	{
+		return amtOfItems;
+	}
+
+	/*Returns the amtOfItems as a int*/
+	Menu::operator int()
+	{
+		return amtOfItems;
+	}
+
+	ostream& Menu::displayTitle(std::ostream& os)
+	{
+		/*Checks to see if menuTitle is null or not and if it's not null it will use the displayName
+		function to display and return the Name datamember representing the name of the object*/
+		if (menuTitle)
+		{
+			menuTitle->displayName(os);
+		}
+		return os;
+	}
+
+	Menu& Menu::operator<<(const char* menuitemConent)
+	{
+		/*Checks to see if theres still room for more Items to be added by comparing amtOfItems
+		already added with the max it can hold.*/
+		if (amtOfItems < MAX_MENU_ITEMS)
+		{
+			/*If there amtOfItems is less than MAX_MENU_ITEMS then memory will
+			be allocated for the new MenuItem object and be initialized by the
+			menuitemContent parameter.*/
+			Items[amtOfItems] = new MenuItem(menuitemConent);
+			amtOfItems++;//amtOfItems will be incremented to represent anouther added item.
+		}
+
+		return *this;
+	}
+
+	/*A overload operator to display the title.*/
+	ostream& operator<<(ostream& os, Menu& title)
+	{
+		return title.displayTitle(os);
+	}
+
+	ostream& Menu::displayMenu(std::ostream& os)
+	{
+		int a = 1;
+		/*Checks to see if menuTitle is valid or not and if so will call the displayName
+		function to display the menuTitles name.*/
+		if (menuTitle)
+		{
+			menuTitle->displayName(os);
+			os << endl;
+		}
+
+		/*Will run through a loop by the amount of items added and using the displayName
+		function will display the Name of the MenuItem.*/
+		for (int i = 0; i < amtOfItems; i++)
+		{
+			os << a << "- ";
+			Items[i]->displayName(os);
+			os << endl;
+			a++;
+		}
+
+		os << "0- Exit" << endl << '>';
+
+		return os;
+	}
+
+	int Menu::run()
+	{
+		int selection = -1;//Variable to hold the entry set to a safe state that cannot be chosen to prevent errors.
+		int flag = 0;
+
+		displayMenu();//Displays the Menu.
+
+		/*A loop that will continue to run till a valid entry is entered.*/
+		while (flag == 0)
+		{
+			/*Checks to see that the cin is valid and if so will test to see if the selection
+			is a valid number*/
+			if (cin >> selection)
+			{
+				if ((selection >= 0) && (selection <= amtOfItems))
+				{
+					flag = 1;//If selection is valid the flag will be set to 1 so that the entry may be returned.
+				}
+				else
+				{
+					cin.clear();          //Safety protocol 
+					cin.ignore(1000, '\n');//for the buffer.
+
+					cout << "Invalid Selection, try again: ";
+					selection = -1;//If the value entered is invalid a error message will prompt the user to empty a valid entry and the selection variable will be set back to a safe state.
+				}
+			}
+			else
+			{
+				cin.clear();            //Safety protocol 
+				cin.ignore(1000, '\n'); //for the buffer.
+				cout << "Invalid Selection, try again: ";
+			}
+		}
+
+		return selection;
+	}
+
+
+	int Menu::operator~()
+	{
+		return run();//Returns current run() output.
+	}
+
+	const char* Menu::operator[](int select)const
+	{
+		if (amtOfItems > 0)//Checks to see that there are any items to begin with.
+		{
+			select = select % amtOfItems; //Using modulus it will get the result of the selection and also if selection is more than the amtOfItems then it will loop back.
+
+			return static_cast<const char*>(*Items[select]);//Returns selected item as const character type.
+		}
+
+		return nullptr; // Return nullptr if there are no items in the menu
+	}
+
+}
+
+
+
+
