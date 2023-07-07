a8c7f162cff555a8f42395f317d57ffb8b95bccc
Wilson Sum <wssum@myseneca.ca>
2023-07-07 17:14:09 -0400

    Minor Fix

--- MS Final Proj/ms2/Utils.cpp
+++ MS Final Proj/ms2/Utils.cpp
@@ -1,6 +1,6 @@
 /*/////////////////////////////////////////////////////////////////////////
 
-Workshop - #MS2
+Workshop - #MS1
 
 Full Name :Wilson Sum
 
@@ -36,9 +36,78 @@ piece of work is entirely of my own creation.
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
-#define _CRT_SECURE_NO_WARNINGS
-#include <iomanip>
-#include <iostream>
-#include <ctype.h>
-using namespace std;
+
 #include "Utils.h"
+namespace sdds {
+	int daysInMonth(int mm)
+	{
+		int days = 0;//This days variable will be used to represent how many days are leading to the month and will be returned. 
+
+		/*This super long list of if statements is to determine how many days are leading into the month determined by the mm parameter.
+		Using multiple if statements over else if statements will prevent confusion to the compiler*/
+		if (mm == 1)
+		{
+			days = 31;
+		}
+
+		if (mm == 2)
+		{
+			days = 31;
+		}
+
+		if (mm == 3)
+		{
+			days = 59;
+		}
+
+		if (mm == 4)
+		{
+			days = 90;
+		}
+
+		if (mm == 5)
+		{
+			days = 120;
+		}
+
+		if (mm == 6)
+		{
+			days = 151;
+		}
+
+		if (mm == 7)
+		{
+			days = 181;
+		}
+
+		if (mm == 8)
+		{
+			days = 212;
+		}
+
+		if (mm == 9)
+		{
+			days = 243;
+		}
+
+		if (mm == 10)
+		{
+			days = 273;
+		}
+
+		if (mm == 11)
+		{
+			days = 304;
+		}
+
+		if (mm == 12)
+		{
+			days = 334;
+		}
+
+		return days;
+	}
+
+}
+
+
