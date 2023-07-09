d7397554c88eac779db50f2f6359e96ef9bbd220
Wilson Sum <wssum@myseneca.ca>
2023-07-09 17:17:43 -0400

    Raw Unofficial form of MS3

---
+++ MS Final Proj/OOPMS3/Publication.cpp
@@ -0,0 +1,43 @@
+/*/////////////////////////////////////////////////////////////////////////
+
+Workshop - #MS3
+
+Full Name :Wilson Sum
+
+Student ID#:017142142
+
+Email :wssum@myseneca.ca
+
+Section :OOP244NAA
+
+Section :OOP244NAA
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+
+shared with any other student or 3rd party content provider. This submitted
+
+piece of work is entirely of my own creation.
+
+/////////////////////////////////////////////////////////////////////////*/
+
+/***********************************************************************
+// OOP244 Utils Module
+// File	Utils.cpp
+// Version
+// Date
+// Author
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+
+namespace sdds {
+	
+
+}
